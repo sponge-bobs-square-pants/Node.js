884c5d2bbea532afe256fe47014d7a6176fc057b
PARTH CHAWLA <82279033+sponge-bobs-square-pants@users.noreply.github.com>
2023-09-28 21:09:08 +0300

    Schema For the DB

--- TASK_MANAGER API/controllers/tasks.js
+++ TASK_MANAGER API/controllers/tasks.js
@@ -1,10 +1,18 @@
-
+const Task = require('../models/task');
 
 const getAllTasks = (req, res) => {
     res.status(200).send('All Items');
 }
-const createTask = (req, res) => {
-    res.status(200).json(req.body);
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body);
+        // res.status(201).json(req.body);
+        res.status(201).json({task});
+    } catch (error) {
+        res.status(500).json({msg : error})
+    }
+   
 }
 const getTask = (req, res) => {
     res.status(200).json({id:req.params.id});
--- TASK_MANAGER API/db/connect.js
+++ TASK_MANAGER API/db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = (url) => {
     mongoose.connect(url, {
         useNewUrlParser:true,
-        useUnifiedTopology:true
+        useUnifiedTopology:true,
     })
 }
 
